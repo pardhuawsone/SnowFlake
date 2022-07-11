094892f947c252c1d290e47f4725744b7a0fd0ba
Makkena <saradhi.Makkena@cdk.com>
2022-07-11 13:06:24 +0530

    cleaned all

--- dbscripts/R__view_View2.sql
+++
@@ -1,2 +0,0 @@
-use schema sampledb.demoschema;
-create view {{ V_N }} as select * from student where GRADE='B';
--- dbscripts/V4.2.4__CreateView1.sql
+++
@@ -1,2 +0,0 @@
-use schema sampledb.demoschema;
-create view view1 as select * from student where GRADE='A';
